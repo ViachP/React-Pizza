5ab8ba8e90149b35b3d54f7289925849f4f98948
Viachaslau Pailak <viachaslau.pailak@gmail.com>
2022-08-03 22:29:24 +0300

    add index.jsx for PizzaBlock

--- react-pizza/src/index.js
+++ react-pizza/src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
 
 import "./scss/app.scss";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
 );
